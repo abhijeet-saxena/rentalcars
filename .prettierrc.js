0c46a5f5c7a2fcbd35bd681c5fdede2fbb589fb8
Abhijeet Saxena <saxenaabhijeet@outlook.com>
2021-05-22 14:53:31 +0530

    chore(app):Setup ESlint and Prettier

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    singleQuote: true,
+    trailingComma: 'all',
+    tabWidth: 4,
+    arrowParens: 'always',
+    useTabs: false,
+    bracketSpacing: true,
+    jsxBracketSameLine: false,
+    printWidth: 80,
+    semi: true,
+};
